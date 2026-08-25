536592b61a2bc0ef6707ba801ec83a3921a52926
godzillazzz <sermpongch@gmail.com>
2026-08-25 22:16:50 +0700

    feat(attendance): mount governance APIs

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,7 @@ const employeeChangeRequestRoutes = require('./employee-change-requests.routes')
 const attendanceDeviceRoutes = require('./attendance-device.routes');
 const { router: attendanceRoutes } = require('./attendance.routes');
 const { router: attendanceSupervisorRoutes } = require('./attendance-supervisor.routes');
+const { router: attendanceGovernanceRoutes } = require('./attendance-governance.routes');
 const employeeReferencePhotoRoutes = require('./employee-reference-photo.routes');
 const { router: faceVerificationRoutes } = require('./face-verification.routes');
 const { router: selfHostedFaceVerificationRoutes } = require('./face-verification-self-hosted.routes');
@@ -24,6 +25,7 @@ router.use('/registration-requests', registrationRequestRoutes);
 router.use('/employee-change-requests', employeeChangeRequestRoutes);
 router.use('/attendance/devices', attendanceDeviceRoutes);
 router.use('/attendance/supervisor', attendanceSupervisorRoutes);
+router.use('/attendance/governance', attendanceGovernanceRoutes);
 router.use('/attendance', attendanceRoutes);
 router.use('/employee-reference-photos', employeeReferencePhotoRoutes);
 router.use('/face-verification', faceVerificationRoutes);
